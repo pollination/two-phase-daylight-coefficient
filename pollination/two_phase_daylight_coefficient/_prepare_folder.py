8a363d5fc7699952031b0fa227c677378e8270f4
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-01-02 13:49:07 +0100

    docs(input): Change description of north

--- pollination/two_phase_daylight_coefficient/_prepare_folder.py
+++ pollination/two_phase_daylight_coefficient/_prepare_folder.py
@@ -21,8 +21,10 @@ class TwoPhasePrepareFolder(GroupedDAG):
     # inputs
     north = Inputs.float(
         default=0,
-        description='A number for rotation from north.',
-        spec={'type': 'number', 'minimum': 0, 'maximum': 360},
+        description='A number between -360 and 360 for the counterclockwise '
+        'difference between the North and the positive Y-axis in degrees. This '
+        'can also be a Vector for the direction to North. (Default: 0).',
+        spec={'type': 'number', 'minimum': -360, 'maximum': 360},
         alias=north_input
     )
 
